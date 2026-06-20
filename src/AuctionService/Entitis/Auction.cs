29cf5b075b9c67c8ea3a48593ead80e3ddf4f57e
Josef <josefleite.00@hotmail.com>
2026-06-20 13:54:12 +0200

    chore: chreated Auction Entiti

---
+++ src/AuctionService/Entitis/Auction.cs
@@ -0,0 +1,19 @@
+namespace AutionService.Entitis;
+
+public class Auction
+{
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; } = 0;
+
+    public string Seller { get; set; }
+    public string Winner { get; set; }
+    public int? SoldAmount { get; set; }
+    public int? CurrentHighestBid { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // UTC time is used to avoid timezone issues
+    public DateTime? EndedAt { get; set; } = DateTime.UtcNow; // Default to UTC time, can be set to null if the auction is still ongoing
+    public DateTime AuctionEnd { get; set; }
+    public Status Status { get; set; }
+    public Item Item { get; set; }
+
+
+}
